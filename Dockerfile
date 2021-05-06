86adba3db5b21308d817b932df6d7a869870c854
ebonynon <sithumsandeepap@gmail.com>
2021-05-06 05:51:38 +0530

    Add support for arm and server; drop support to windows

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,23 @@
 FROM golang:1.16.3-alpine3.13 as builder
 
+RUN apk update && apk add gcc g++ musl-dev
+
 RUN mkdir /app
 
 COPY . /app
 
 WORKDIR /app
 
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o movie-box .
+RUN go build -ldflags="-w -s" -o movie-box .
 
 FROM alpine:3.13.4
 
 WORKDIR /app
 
 COPY --from=builder /app/movie-box /usr/local/bin/
+
+EXPOSE 3080
+
+CMD [ "movie-box" , "server" ]
 
 ENTRYPOINT [ "movie-box" , "get" ]
--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,7 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-w -s" -o build/movie-box_0.0.1_linux_386 .
+rm -rf build/*
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/movie-box_0.0.1_linux_amd64 .
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags="-w -s" -o build/movie-box_0.0.1_windows_386 .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o build/movie-box_0.0.1_windows_amd64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-w -s" -o build/movie-box_0.0.1_linux_arm .
+docker buildx build --platform linux/amd64,linux/arm/v7 -t ghcr.io/sithumonline/movie-box:0.0.4 \
+      --cache-to=type=registry,ref=ghcr.io/sithumonline/movie-box:0.0.4-cache \
+      --cache-from=type=registry,ref=ghcr.io/sithumonline/movie-box:0.0.4-cache \
+      --push .
