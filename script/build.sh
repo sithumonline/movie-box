15c12eb01c2efa0188911868da914aba55a7061a
ebonynon <sithumsandeepap@gmail.com>
2021-05-07 09:58:32 +0530

    Set var version

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,8 @@
 rm -rf build/*
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/movie-box_0.0.1_linux_amd64 .
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-w -s" -o build/movie-box_0.0.1_linux_arm .
-docker buildx build --platform linux/amd64,linux/arm/v7 -t ghcr.io/sithumonline/movie-box:0.0.4 \
-      --cache-to=type=registry,ref=ghcr.io/sithumonline/movie-box:0.0.4-cache \
-      --cache-from=type=registry,ref=ghcr.io/sithumonline/movie-box:0.0.4-cache \
+export VERSION=0.0.4
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/movie-box_$VERSION_linux_amd64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-w -s" -o build/movie-box_$VERSION_linux_arm .
+docker buildx build --platform linux/amd64,linux/arm/v7 -t ghcr.io/sithumonline/movie-box:$VERSION \
+      --cache-to=type=registry,ref=ghcr.io/sithumonline/movie-box:$VERSION-cache \
+      --cache-from=type=registry,ref=ghcr.io/sithumonline/movie-box:$VERSION-cache \
       --push .
